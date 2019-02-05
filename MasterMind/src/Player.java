32c7607bccb33d8faa8cff6f2f6c4a935cabe167
dorianfe <fenestre.dorian@free.fr>
2019-02-05 12:56:05 +0100

    création vérification combinaison

--- MasterMind/src/Player.java
+++ MasterMind/src/Player.java
@@ -1,40 +1,42 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.ListIterator;
 import java.util.Scanner;
 
 public class Player {
 
     private Scanner sc = new Scanner(System.in);
+    private int x; // représente le nb d'entiers dans la combinaison
+    private int chiffre;
 
-
-    //public void Combinaison() {
-    //    System.out.println("De combien de chiffres de 0 à 9 votre combinaison est-elle constituée ? ");
-    //    int x = sc.nextInt();
-    //    List<Integer> chiffres = null;
-    //    for (int i = 1; i <= x; i++) {
-    //        System.out.println("Choisissez un chiffre :");
-    //        int chiffre = sc.nextInt();
-    //        chiffres = new ArrayList<Integer>();
-    //        chiffres.add(chiffre);
-    //    }
-    //}
 
     public List<Integer> createCombinaison() {
         System.out.println("De combien de chiffres de 0 à 9 votre combinaison est-elle constituée ? ");
-        int x = sc.nextInt();
+        x = sc.nextInt();
         List<Integer> chiffres = new ArrayList<Integer>();
         for (int i = 1; i <= x; i++) {
             System.out.println("Choisissez un chiffre :");
-            int chiffre = sc.nextInt();
+            chiffre = sc.nextInt();
             chiffres.add(chiffre);
         }
-        return (chiffres);
+        return combinaison();
     }
 
-    public void combinaison(){
+    public void combinaison() {
+        System.out.println("Veuillez faire une proposition de chiffre");
+        int c = sc.nextInt();
+        for (int b = 1; b <= x; b++) {
+            createCombinaison().get(chiffre);
+            if (c < chiffre) {
+                System.out.println("+");
+            }
+                else if (c == chiffre) {
+                System.out.println("=");
+        } else  {
+                System.out.println("-");
+            }
 
     }
+}
 
 
 }
