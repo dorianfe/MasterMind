4caae3d1c507d9754e0f2cf42789e5fddf8495d3
dorianfe <fenestre.dorian@free.fr>
2019-02-13 15:52:57 +0100

    classe plusmoins crée.

---
+++ MasterMind/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        PlusMoins plusMoins = new PlusMoins();
+        plusMoins.init();
+        plusMoins.proposition();
+        plusMoins.verifCombi();
+
+
+    }
+}
