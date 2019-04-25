1a887db529bf799a3a9002c8a341b92362b884e0
dorianfe <fenestre.dorian@free.fr>
2019-04-25 17:53:27 +0200

    ajout nbessais duel

--- MasterMind/src/game/Duel.java
+++ MasterMind/src/game/Duel.java
@@ -3,7 +3,7 @@ package game;
 import java.util.Arrays;
 
 public class Duel extends Mode {
-    private int nbEssais;
+    private int nbEssais = 0;
     private boolean alreadyExecuted;
     private int[] propositionHum;
     private int[] propositionIaFinale;
@@ -16,20 +16,22 @@ public class Duel extends Mode {
             do {
                 System.out.println("Joueur Humain, faites une proposition");
                 propositionHum = convertir(saisir());
+                nbEssais++;
                 verifCombi(combinaison, propositionHum, 0);
-                System.out.println(Arrays.toString(propositionHum) + "indice : " + indice);
+                System.out.println(Arrays.toString(propositionHum) + "indice : " + indice + " Essais: " + nbEssais);
                 if (!alreadyExecuted) {
                     propositionIa = convertir(propositionInit());
                     alreadyExecuted = true;
                     verifCombi(combinaison, propositionIa, 0);
                     indiceIa = indice;
-                    System.out.println(Arrays.toString(propositionIa) + "indiceIa : " + indiceIa);
+                    System.out.println(Arrays.toString(propositionIa) + "indiceIa : " + indiceIa + " Essais: " + nbEssais);
                 } else {
                     verifCombi(combinaison, computerTest(indiceIa, 0), 0);
                     indiceIa = indice;
-                    System.out.println(Arrays.toString(propositionIa) + "indiceIa : " + indiceIa);
+                    System.out.println(Arrays.toString(propositionIa) + "indiceIa : " + indiceIa + " Essais: " + nbEssais);
                     propositionIaFinale = propositionIa;
                 }
+
                 /*if (propositionIaFinale == propositionHum) { // ne fonctione pas, dans ou en dehors de la boucle while
                     System.out.println("ex-aequo!");
                 } else if (combinaison == propositionIaFinale) {
