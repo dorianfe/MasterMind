a10d667624c1b9c20a1ad13fea0f188cc4b779e9
dorianfe <fenestre.dorian@free.fr>
2019-04-29 12:02:40 +0200

    amélioration affichage indice essais proposition Challenger

--- MasterMind/src/game/Challenger.java
+++ MasterMind/src/game/Challenger.java
@@ -17,15 +17,25 @@ public class Challenger extends Mode {
                 proposition = convertir(saisir());
                 nbEssais++;
                 verifCombi(combinaison, proposition, 0);
-                System.out.println(Arrays.toString(proposition) + "indice : " + indice + " essais: " + nbEssais);
+                //System.out.println(Arrays.toString(proposition) + "indice : " + indice + " essais: " + nbEssais);
+                System.out.println(("indice: " + indice + " essais: " + nbEssais + " proposition ") + Arrays.toString(proposition));
                 if (nbEssais == 10){
                     System.out.println("Perdu, nombre d'essais max atteint!");
                     break;
                 }
             } while (!Arrays.equals(combinaison, proposition));
+            System.out.println("Victoire en " + nbEssais + " essais.");
 
         }else {
             System.out.println("-----------");
+            combinaison = convertir(rdmProposition());
+            nbEssais = 0;
+            do {
+                System.out.println("Faites une proposition");
+                proposition = convertir(saisir());
+                nbEssais++;
+                verifCombi(combinaison, proposition,1);
+            } while (!Arrays.equals(combinaison, proposition));
         }
     }
 }
