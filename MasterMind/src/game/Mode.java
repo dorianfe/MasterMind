5040b5d63d4397105c0a4fcc0584195f5fd66a3f
dorianfe <fenestre.dorian@free.fr>
2019-05-07 17:54:52 +0200

    avancée IA mastermind, liste des combinaisons possibles, lien entre indices et propositionIA

--- MasterMind/src/game/Mode.java
+++ MasterMind/src/game/Mode.java
@@ -9,7 +9,9 @@ public abstract class Mode {
     protected int[] derniereTentative;
     protected int[] combinaison;
     protected int[] propositionIa;
-    private ArrayList<Integer> tentative = new ArrayList<Integer>();
+    List<Integer> _PossibleTokens; //combinaisons possibles
+    List<Integer> tentatives;//liste pour stocker les tentatives mastermind
+    int[] _ValidDigits = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}; //chiffres utilisables
     protected String indice;
     private int turn;
 
@@ -111,25 +113,49 @@ public abstract class Mode {
                     - Si X<A
             - Si (9-A)%2 = 0 alors X = X + (9-A)/2  sinon X = X + (9-A)/2 - 1 */
         } else {
-            //knuth pour mastermind ?
+            //propositionIa = convertir(1122);
 
-            int B = indice.charAt(2);
-            int W = indice.charAt(4);
-            if (B == 0 && W == 0) {
+            _PossibleTokens = GetAllPossibleTokens();
+
+
+
+
+            //int B = indice.charAt(2);
+            //int W = indice.charAt(4);
+
+            /*if (B == 0 && W == 0) {
                 propositionIa = convertir(3344);
             } else {
                 switch (B) {
                     case 1:
+                        propositionIa =
 
                 }
             }
-            tentative.add(convertir(propositionIa), turn);
+
+          */
+            tentatives.add(convertir(propositionIa), turn);
             turn++;
             return propositionIa;
 
 
         }
         return propositionIa;
+    }
+
+    private List<Integer> GetAllPossibleTokens() {
+        List<Integer> tokens = new ArrayList<>();
+        for (int d1 = 0; d1 < _ValidDigits.length; d1++)
+            for (int d2 = 0; d2 < _ValidDigits.length; d2++)
+                for (int d3 = 0; d3 < _ValidDigits.length; d3++)
+                    for (int d4 = 0; d4 < _ValidDigits.length; d4++) {
+                        if (d1 != d2 && d1 != d3 && d1 != d4
+                                && d2 != d3 && d2 != d4
+                                && d3 != d4) {
+                            tokens.add((_ValidDigits[d1] + _ValidDigits[d2] + _ValidDigits[d3] + _ValidDigits[d4]));
+                        }
+                    }
+        return tokens;
     }
 
 
